65de18e8968fefcdefa7b09bf39a4c4b20a2659a
sanchayana2007 <sanchayana2007@gmail.com>
2014-05-26 17:00:17 +0530

    d
    d

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,17 @@
+
+var mongoose = require( 'mongoose' );
+var Todo     = mongoose.model( 'Todo' );
+
 exports.index = function ( req, res ){
   res.render( 'index', { title : 'Express Todo Example' });
+};
+
+
+exports.create = function ( req, res ){
+  new Todo({
+    content    : req.body.content,
+    updated_at : Date.now()
+  }).save( function( err, todo, count ){
+    res.redirect( '/' );
+  });
 };
