65f07da68886d6f567f6163ebc1cec5205889257
sanchayana2007 <sanchayana2007@gmail.com>
2014-05-26 17:13:13 +0530

    added
    dd

--- routes/index.js
+++ routes/index.js
@@ -15,3 +15,69 @@ exports.create = function ( req, res ){
     res.redirect( '/' );
   });
 };
+
+// query db for all todo items
+exports.index = function ( req, res ){
+  Todo.find( function ( err, todos, count ){
+    res.render( 'index', {
+      title : 'Express Todo Example',
+      todos : todos
+    });
+  });
+};
+
+// remove todo item by its id
+exports.destroy = function ( req, res ){
+  Todo.findById( req.params.id, function ( err, todo ){
+    todo.remove( function ( err, todo ){
+      res.redirect( '/' );
+    });
+  });
+};
+
+exports.edit = function ( req, res ){
+  Todo.find( function ( err, todos ){
+    res.render( 'edit', {
+        title   : 'Express Todo Example',
+        todos   : todos,
+        current : req.params.id
+    });
+  });
+};
+
+
+// redirect to index when finish
+exports.update = function ( req, res ){
+  Todo.findById( req.params.id, function ( err, todo ){
+    todo.content    = req.body.content;
+    todo.updated_at = Date.now();
+    todo.save( function ( err, todo, count ){
+      res.redirect( '/' );
+    });
+  });
+};
+
+exports.index = function ( req, res ){
+  Todo.
+    find().
+    sort( '-updated_at' ).
+    exec( function ( err, todos ){
+      res.render( 'index', {
+          title : 'Express Todo Example',
+          todos : todos
+      });
+    });
+};
+ 
+exports.edit = function ( req, res ){
+  Todo.
+    find().
+    sort( '-updated_at' ).
+    exec( function ( err, todos ){
+      res.render( 'edit', {
+          title   : 'Express Todo Example',
+          todos   : todos,
+          current : req.params.id
+      });
+    });
+};
